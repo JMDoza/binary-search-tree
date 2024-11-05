c55b2a0bab9b13f8e3d895497802a4a8657a3d64
JMDoza <66657082+JMDoza@users.noreply.github.com>
2024-11-05 05:44:47 -0500

    added recursive build tree method and pretty print function to visualize tree

--- bts.js
+++ bts.js
@@ -2,10 +2,6 @@ import { mergeSort } from "./mergesort.js";
 function Tree(array) {
   let root = null;
 
-  if (array) {
-    console.log(buildTree(array));
-  }
-
   const Node = (data) => {
     return {
       data,
@@ -14,12 +10,30 @@ function Tree(array) {
     };
   };
 
-  function buildTree(array) {
-    let sortedArray = mergeSort(array);
-    sortedArray = removeDupes(sortedArray);
+  const getRoot = () => {
+    return root;
+  };
 
-    return sortedArray;
+  const buildTree = (array, start, end) => {
+    if (start > end) {
+      return null;
+    }
+    const mid = Math.floor((start + end) / 2);
+
+    const rootNode = Node(array[mid]);
+    rootNode.leftNode = buildTree(array, start, mid - 1);
+    rootNode.rightNode = buildTree(array, mid + 1, end);
+
+    return rootNode;
+  };
+
+  if (array) {
+    array = mergeSort(array);
+    array = removeDupes(array);
+    root = buildTree(array, 0, array.length - 1);
   }
+
+  return { getRoot };
 }
 
 function removeDupes(array) {
@@ -31,7 +45,22 @@ function removeDupes(array) {
       currentDupe = array[i];
     }
   }
+  console.log(array);
   return array;
 }
 
-Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+function prettyPrint(node, prefix = "", isLeft = true) {
+  if (node === null) {
+    return;
+  }
+  if (node.rightNode !== null) {
+    prettyPrint(node.rightNode, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.leftNode !== null) {
+    prettyPrint(node.leftNode, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+}
+
+let tree = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(tree.getRoot());
